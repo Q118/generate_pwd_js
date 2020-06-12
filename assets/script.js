680e52ccf8e20197765be71b99460fd422b82028
Q118 <shelbyfish91@gmail.com>
2020-06-11 18:02:35 -0700

    parameters for special characters

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,48 @@
+/** @format */
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
-var length = prompt("How many characters would you like your password to contain?");
+    //asking for length
+	var length = prompt(
+		"How many characters would you like your password to contain?"
+	);
+
+	if (length > 8 && length < 128) {
+        //asking for character types
+		var special = confirm("Click OK to include special characters.");
+	    var numeric = confirm("Click OK to include numeric characters.");
+        var lowerCase = confirm("Click OK to include lower-case characters.");
+        var upperCase = confirm("Click OK to include upper-case characters.");
+    //looking at user input
+        var charSet = "";
+        if(special) {
+            charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        }
 
 
 
-if (length > 8 && length < 128) {
-    var special = confirm("Click OK to include special characters.");
 
-var numeric = confirm("Click OK to include numeric characters.");
 
-var lowerCase = confirm("Click OK to include lower-case characters.");
 
-var upperCase = confirm("Click OK to include upper-case characters.");
+
+
+	} else if (length < 8 || length > 128) {
+		alert("Please choose a number between 8 and 128!");
+	} else {
+		alert("Please choose a number between 8 and 128!");
+	}
+
+	//var password = generatePassword();
+	//var passwordText = document.querySelector("#password");
+
+	//passwordText.value = password;
 }
-else if (length < 8 || length > 128) {
-    alert("Please choose a number between 8 and 128!");
-}
-else {
-    alert("Please choose a number between 8 and 128!");
-}
-
-
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 /*function generatePassword() {
     var length = 8,
